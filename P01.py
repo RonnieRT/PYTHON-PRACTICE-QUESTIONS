64f5abb7db39f3de509ee96f650407fcc501bb61
RonnieRT <98645458+RonnieRT@users.noreply.github.com>
2023-04-10 23:25:23 +0530

    Easy Pronunciation
    Question link is https://www.codechef.com/problems/EZSPEAK

---
+++ P01.py
@@ -0,0 +1,19 @@
+# PROGRAM FOR EASY PRONUNCIATION
+for t in range(int(input())):
+    n = int(input())
+    s = input()
+    if n<=3:
+        print("YES")
+    else:
+        for i in range(n-3):
+            string = s[i:i+4]
+            vow1 = 'a' in string or 'e' in string or 'i' in string 
+            vow2 = 'o' in string or 'u' in string 
+            if not vow1 and not vow2:
+                print("NO")
+                break 
+        else:
+            print("YES")
+        
+    
+        
