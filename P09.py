2121684a6ec022bc46a375e62a4473af50a39327
RonnieRT <98645458+RonnieRT@users.noreply.github.com>
2023-04-13 22:26:03 +0530

    Easy Permutation
    Question is : - codechef.com/problems/EASYPERM?tab=submissions

---
+++ P09.py
@@ -0,0 +1,18 @@
+# cook your dish here
+for i in range(int(input())):
+    n = [x for x in range(1,int(input())+1)]
+    print(*n[::-1])
+    
+    
+    
+    
+    or 
+    
+    
+    
+    
+# cook your dish here
+n = int(input())
+for i in range(n):
+    m = int(input())
+    print(*range(m,0,-1))
