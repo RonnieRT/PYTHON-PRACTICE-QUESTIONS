6a760185060906b65f0b2c00b6f0ead412ac4119
RonnieRT <98645458+RonnieRT@users.noreply.github.com>
2023-04-18 23:09:18 +0530

    Alternating String
    Link of the question is :- https://www.codechef.com/problems/ALTSTR?tab=submissions

---
+++ P16.py
@@ -0,0 +1,9 @@
+# cook your dish here
+for _ in range(int(input())):
+    n = int(input())
+    s = input()
+    ones = s.count('1')
+    zeros = n - ones
+    pairs = 2*min(ones,zeros)
+    extra = min(abs(zeros - ones),1)
+    print(pairs + extra)
