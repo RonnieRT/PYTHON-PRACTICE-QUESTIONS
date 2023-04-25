23168117121f0159005df6477bb525630d3afd83
RonnieRT <98645458+RonnieRT@users.noreply.github.com>
2023-04-25 22:42:38 +0530

    Sort The String
    Question Link:- https://www.codechef.com/problems/SRTARR

---
+++ P27.py
@@ -0,0 +1,9 @@
+ # cook your dish here
+for i in range(int(input())):
+    n = int(input())
+    s = input()
+    count = 0
+    for i in range(0, n-1):
+        if s[i:i+2] == "10":
+            count += 1
+    print(count)
