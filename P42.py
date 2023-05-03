e675e3a228f9453e79f815f3c2e704ef0511bfbf
RonnieRT <98645458+RonnieRT@users.noreply.github.com>
2023-05-03 21:03:05 +0530

    Masterchef Finals
    Question Link : - https://www.codechef.com/START88D/problems/TOP10?tab=statement

---
+++ P42.py
@@ -0,0 +1,25 @@
+# cook your dish here
+t = int(input())
+for i in range(t):
+    a = int(input())
+    if a < 11 and a > 0:
+        print("YES")
+    else:
+        print("NO")
+    
+    
+    # 2nd Soltuions 
+    
+    # cook your dish here
+for i in range(int(input())):
+    a = int(input())
+    if a < 11 and a > 0:
+        print("YES")
+    else:
+        print("NO")
+    
+    
+    
+
+    
+    
