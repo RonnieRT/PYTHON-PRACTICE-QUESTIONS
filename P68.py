5d3a5f8ea3b3e080c409ec4ed9c82bb820402faa
RonnieRT <98645458+RonnieRT@users.noreply.github.com>
2023-05-20 15:05:20 +0530

    Rain In Chefland
    Question Link : https://www.codechef.com/problems/RAINFALL1?tab=statement

---
+++ P68.py
@@ -0,0 +1,11 @@
+# cook your dish here
+for i in range(int(input())):
+    a = int(input())
+    if(a<3):
+        print("LIGHT")
+    elif(a>=3 and a<7):
+        print("MODERATE")
+    else:
+        print("HEAVY")
+        
+    
