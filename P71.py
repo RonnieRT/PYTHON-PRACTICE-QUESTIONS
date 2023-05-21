0c40b45fba9098e0bd9f5f85d2f7e6fe7a86fd3c
RonnieRT <98645458+RonnieRT@users.noreply.github.com>
2023-05-22 01:04:13 +0530

    Chess Time
    Question Link :- https://www.codechef.com/problems/CHESSTIME?tab=submissions

---
+++ P71.py
@@ -0,0 +1,12 @@
+# cook your dish here
+t=int(input())
+for i in range(t):
+    x=int(input())
+    print(x*3)
+    
+    OR 
+     
+t = int(input())
+for i in range(t):
+    N = int(input())
+    print((N*60)//20)
